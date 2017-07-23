402e498651f6e064ede32f8243554eae1eab5eb7
kotori <644497572@qq.com>
2017-07-23 18:30:11 +0800

    jsya

--- sboot/src/test/java/cc/kotori/sboot/utils/MailServiceTest.java
+++ sboot/src/test/java/cc/kotori/sboot/utils/MailServiceTest.java
@@ -2,13 +2,14 @@ package cc.kotori.sboot.utils;
 
 import javax.annotation.Resource;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class MailServiceTest {
 
@@ -20,5 +21,12 @@ public class MailServiceTest {
         mailService.sendSimpleMail("644497572@qq.com","test simple mail"," hello this is simple mail");
     }
     
-    
+    @Autowired
+    StringEncryptor stringEncryptor;
+
+    @Test
+    public void encryptPwd() {
+        String result = stringEncryptor.encrypt("yourpassword");
+        System.out.println(result); 
+    }
 }
